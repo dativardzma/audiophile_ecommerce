58d78b65d76a6ed302de43680cd633164b3a3733
dativardzma <dati.svanidze123@gmail.com>
2025-06-30 21:26:26 +0400

    changed login functionaly

--- eccomerce/app/functions.py
+++ eccomerce/app/functions.py
@@ -2,16 +2,13 @@ from django.contrib.auth.hashers import check_password
 from .models import CustomUser
 
 def user_login_function(email=None, password=None):
-    """
-    Function to authenticate a user with email and password.
-    Returns the user object if authentication is successful, otherwise None.
-    """
     try:
         user = CustomUser.objects.get(email=email)
+        if not user.is_active:
+            return None
     except CustomUser.DoesNotExist:
         return None
 
     if check_password(password, user.password):
         return user
-        print(user)
     return None
